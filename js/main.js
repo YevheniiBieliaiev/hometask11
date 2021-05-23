944bcf5959b6396150a1fa898c43c7531f11ce7f
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2021-05-23 14:05:11 +0300

    gallery content - links

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-document.addEventListener( 'DOMContentLoaded', function () {
-	new Splide( '.splide' ).mount();
-} );
+document.addEventListener('DOMContentLoaded', function () {
+	new Splide('.splide').mount();
+});
+
+
 
 var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 1000
