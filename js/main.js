90b5d815a259f0ae13e975d73e5a797e81e14286
Yevhenii Bieliaiev <yevheniibieliaiev@gmail.com>
2021-05-23 19:31:45 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-	new Splide('.splide').mount();
+	new Splide('.splide', {
+		type: 'loop',
+		autoplay: true,
+		interval: 4000,
+		pauseOnHover: false
+	}).mount();
 });
-
-
 
 var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 1000
